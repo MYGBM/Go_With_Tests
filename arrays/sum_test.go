e590ef2d9a7f685681d32ba815b1630fc12a815a
Mariam Gustavo <yegetamariam@gmail.com>
2023-08-19 12:41:56 +0300

    added checkSum function to check the equality of slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -24,19 +24,24 @@ func TestSumAll(t *testing.T) {
 
 }
 func TestSumAllTails(t *testing.T) {
-	t.Run("make the sum of some slices", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2, 3}, []int{4, 5, 6})
-		want := []int{5, 11}
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
-	})
-	t.Run("safely sum empty slices", func(t *testing.T) {
-		got := SumAllTails([]int{}, []int{4, 5, 6})
-		want := []int{0, 11}
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
 
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
 	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
 }
