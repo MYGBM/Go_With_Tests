43c669c6ea97e90846e70e12a8def35e36c77a8a
Mariam Gustavo <yegetamariam@gmail.com>
2023-08-23 10:03:56 +0300

    Searching for known and unknown word in a dictionary

---
+++ maps/maps_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want, "test")
+
+	})
+	t.Run("unknown word ", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+		assertErrors(t, got, ErrNotFound)
+	})
+
+}
+func assertStrings(t testing.TB, got, want, given string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q given %q ", got, want, given)
+	}
+}
+func assertErrors(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
